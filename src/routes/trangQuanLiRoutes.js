54a25790d5dcac96163910adb7bd0fd7f84a20ac
VietDucTN03 <vietductn281103@gmail.com>
2024-01-31 17:31:50 +0700

    update

--- src/routes/trangQuanLiRoutes.js
+++ src/routes/trangQuanLiRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { deleteImageByID, getImgCreatedByUserID, getImgSavedByUserID, getInfoUser,  } from '../controllers/trangQuanLiController.js';
 import { khoaAPI } from '../config/jwt.js';
+import { updateInfoUser } from '../controllers/trangChinhSuaTTCN.js';
 
 const trangQuanLiRoutes = express.Router();
 
@@ -8,5 +9,6 @@ trangQuanLiRoutes.get("/getInfoUser", khoaAPI, getInfoUser);
 trangQuanLiRoutes.get("/getImgSavedByUserID/:userID", khoaAPI, getImgSavedByUserID);
 trangQuanLiRoutes.get("/getImgCreatedByUserID/:userID", khoaAPI, getImgCreatedByUserID);
 trangQuanLiRoutes.delete("/deleteImageByID/:imageID", khoaAPI, deleteImageByID);
+trangQuanLiRoutes.put("/update-user/:userID", khoaAPI, updateInfoUser);
 
 export default trangQuanLiRoutes
